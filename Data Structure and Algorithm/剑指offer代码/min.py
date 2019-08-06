65c8b584ae680ed4d7900ace85b1ab6c02d48a25
Microstrong <zhouyanqiang0305@qq.com>
2019-08-07 01:56:21 +0800

    [add] isPopOrder

--- Data Structure and Algorithm/剑指offer代码/min.py
+++ Data Structure and Algorithm/剑指offer代码/min.py
@@ -6,27 +6,54 @@ class Solution:
     def __init__(self):
         self.stack = []
         self.minStack = []
-        self.minm = float("inf")
+        self.min_num = None
 
     def push(self, node):
         # write code here
-        if node < self.minm:
-            self.minm = node
-            self.minStack.append(self.minm)
-        self.minStack.append(node)
+        self.stack.append(node)
+        if self.min_num is None:
+            self.min_num = node
+            self.minStack.append(self.min_num)
+        elif self.min_num < node:
+            self.minStack.append(self.min_num)
+        else:
+            self.min_num = node
+            self.minStack.append(node)
 
     def pop(self):
         # write code here
-        if self.stack != []:
-            if self.stack[-1] == self.minm:
-                self.minStack.pop()
-            self.stack.pop(-1)
+        if len(self.stack) > 0 and len(self.minStack):
+            self.minStack.pop()
+            self.stack.pop()
 
     def top(self):
         # write code here
-        if self.stack != []:
-            return self.stack[-1]
+        return
 
     def min(self):
         # write code here
         return self.minStack[-1]
+
+if __name__ == '__main__':
+    stack = Solution()
+    current_stack = [11, 7, 8, 9, 5, 12, 15]
+    for num in (current_stack):
+        stack.push(num)
+    print(stack.min(), stack.stack)
+    print(stack.minStack)
+    # 测试用例
+    # 输入一个比最小值大的
+    stack.push(7)
+    print(stack.min(), stack.stack)
+
+    # 输入一个比最小值小的
+    stack.push(4)
+    print(stack.min(), stack.stack)
+
+    # 弹出最小值
+    stack.pop()
+    print(stack.min(), stack.stack)
+
+    # 弹出非最小值
+    stack.pop()
+    print(stack.min(), stack.stack)
