544ee7c53fa4f2b1c8fb28c0c70998d28d2ee9c8
zhouyanqiang <zhouyanqiang@onething.net>
2020-02-28 18:00:55 +0800

    [add] 逆序对

--- Data Structure and Algorithm/剑指offer代码/数组中的逆序对/InversePairs.py
+++ Data Structure and Algorithm/剑指offer代码/数组中的逆序对/InversePairs.py
@@ -1,89 +1,85 @@
-# class Solution:
-#
-#     def InversePairs(self, data):
-#         data_len = len(data)
-#         if data is None or data_len == 0:
-#             return None
-#
-#         count = 0
-#         for i in range(data_len):
-#             for j in range(i + 1, data_len):
-#                 if data[i] > data[j]:
-#                     count += 1
-#         return count
-#
-#     def InversePairs2(self, data):
-#         if data is None or len(data) <= 0:
-#             return 0
-#
-#         new_data = data
-#         count = self.InversePairsCore(data, new_data, 0, len(data))
-#
-#         return count
-#
-#     def InversePairsCore(self, data, new_data, low, high):
-#         if low == high:
-#             new_data[low] = data[low]
-#             return 0
-#
-#         len = (high - low) / 2
-#         left = self.InversePairsCore(new_data, data, low, low + len) % 1000000007
-#         right = self.InversePairsCore(new_data, data, low + len + 1, high) % 1000000007
-#
-#         # p1 初始化为前半段最后一个数字的下标
-#         p1 = low + len
-#         # p2 初始化为后半段最后一个数字的下标
-#         p2 = high
-#         # 开始拷贝的位置
-#         p3 = high
-#         # 逆序数
-#         count = 0
-#         while p1 >= low and p2 >= low + len + 1:
-#             if data[p1] > data[p2]:
-#                 # 对应的逆序数
-#                 count += (p2 - low - len)
-#                 new_data[p3] = data[p1]
-#                 p3 -= 1
-#                 p1 -= 1
-#                 if count >= 1000000007:
-#                     count %= 1000000007
-#                 else:
-#                     new_data[p3] = data[p2]
-#                     p3 -= 1
-#                     p2 -= 1
-#
-#         while p1 >= low:
-#             new_data[p3] = data[p1]
-#             p3 -= 1
-#             p1 -= 1
-#
-#         while p2 >= low + len + 1:
-#             new_data[p3] = data[p2]
-#             p3 -= 1
-#             p2 -= 1
-#
-#         return (count + left + right) % 1000000007
-#
-#
-# if __name__ == "__main__":
-#     input_data = [7, 5, 6, 4]
-#     sol = Solution()
-#     print(sol.InversePairs2(input_data))
-
-
 class Solution:
 
     def InversePairs(self, data):
-        count = 0
-        for index, value in enumerate(data):
-            for i in range(index + 1, len(data)):
-                if value > data[i]:
-                    count += 1
+        if data is None or len(data) <= 0:
+            return 0
+
+        copy = [i for i in data]
+
+        count = self.InversePairsCore(data, copy, 0, len(data) - 1)
 
         return count % 1000000007
 
+    def InversePairsCore(self, data, copy, start, end):
+        if start == end:
+            copy[start] = data[start]
+            return 0
+
+        length = (end - start) // 2
+        left = self.InversePairsCore(copy, data, start, start + length)
+        right = self.InversePairsCore(copy, data, start + length + 1, end)
+
+        # p1 初始化为前半段最后一个数字的下标
+        p1 = start + length
+        # p2 初始化为后半段最后一个数字的下标
+        p2 = end
+        # 开始拷贝的位置
+        p3 = end
+        # 逆序数
+        count = 0
+
+        # 对两个数组进行对比取值的过程
+        while p1 >= start and p2 >= start + length + 1:
+            if data[p1] > data[p2]:
+                copy[p3] = data[p1]
+                p3 -= 1
+                p1 -= 1
+                # 对应的逆序数
+                count += p2 - start - length
+            else:
+                copy[p3] = data[p2]
+                p3 -= 1
+                p2 -= 1
+
+        # 剩下的一个数组未取完的操作
+        while p1 >= start:
+            copy[p3] = data[p1]
+            p3 -= 1
+            p1 -= 1
+
+        while p2 >= start + length + 1:
+            copy[p3] = data[p2]
+            p3 -= 1
+            p2 -= 1
+
+        return count + left + right
+
 
 if __name__ == "__main__":
+    # input_data = [7, 5, 6, 4]
+    input_data = [1, 2, 3, 4, 5, 6, 7, 0]
     sol = Solution()
-    data_list = [1, 2, 3, 4, 5, 6, 7, 0]
-    print(sol.InversePairs(data_list))
+    print(sol.InversePairs(input_data))
+
+# class Solution:
+#
+#     def InversePairs(self, data):
+#         count = 0
+#         for index, value in enumerate(data):
+#             for i in range(index + 1, len(data)):
+#                 if value > data[i]:
+#                     count += 1
+#
+#         return count % 1000000007
+#
+#
+# if __name__ == "__main__":
+#     sol = Solution()
+#     data_list = [1, 2, 3, 4, 5, 6, 7, 0]
+#     print(sol.InversePairs(data_list))
+
+
+'''
+Reference：
+【1】https://github.com/leeguandong/Interview-code-practice-python/blob/master/%E5%89%91%E6%8C%87offer/%E6%95%B0%E7%BB%84%E4%B8%AD%E7%9A%84%E9%80%86%E5%BA%8F%E5%AF%B9.py
+'''
