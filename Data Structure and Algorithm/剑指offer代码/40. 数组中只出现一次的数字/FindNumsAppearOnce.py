ee4fd081b5fa433ad9d6338c3f71559e54dad519
Microstrong <zhouyanqiang0305@qq.com>
2020-03-04 09:54:45 +0800

    [add] 数组中只出现一次的数

--- Data Structure and Algorithm/剑指offer代码/40. 数组中只出现一次的数字/FindNumsAppearOnce.py
+++ Data Structure and Algorithm/剑指offer代码/40. 数组中只出现一次的数字/FindNumsAppearOnce.py
@@ -1,32 +1,52 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    # 返回[a,b] 其中ab是出现一次的两个数字
-    # def FindNumsAppearOnce(self, array):
-    #     # write code here
-    #     num_dic = {}
-    #     result_list = []
-    #     for item in array:
-    #         if item in num_dic.keys():
-    #             num_dic[item] = num_dic[item] + 1
-    #         else:
-    #             num_dic[item] = 1
-    #
-    #     for item, count in num_dic.items():
-    #         if count == 1:
-    #             result_list.append(item)
-    #
-    #     return result_list
+
+
+# 返回[a,b] 其中ab是出现一次的两个数字
+# def FindNumsAppearOnce(self, array):
+#     # write code here
+#     num_dic = {}
+#     result_list = []
+#     for item in array:
+#         if item in num_dic.keys():
+#             num_dic[item] = num_dic[item] + 1
+#         else:
+#             num_dic[item] = 1
+#
+#     for item, count in num_dic.items():
+#         if count == 1:
+#             result_list.append(item)
+#
+#     return result_list
+
+
+# def FindNumsAppearOnce(self, array):
+#     # write code here
+#     result_list = []
+#     for item in array:
+#         count = array.count(item)
+#         if count == 1:
+#             result_list.append(item)
+#
+#     return result_list
 
 
     def FindNumsAppearOnce(self, array):
         # write code here
-        result_list = []
-        for item in array:
-            count = array.count(item)
-            if count == 1:
-                result_list.append(item)
+        if array == None or len(array) < 2:
+            return []
 
-        return result_list
+        # 对数据进行异或运算
+        resultExclusiveOR = 0
+        for i in array:
+            resultExclusiveOR ^=i
+
+        # 在异或结果中找出从右向左第一个为1的索引
+        indexOf1 = self.FindFirstBitIs1()
+
+    def FindFirstBitIs1(self, num):
+        indexBit = 0
+        while
 
 
 
