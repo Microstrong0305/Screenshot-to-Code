901977fac456a22ab844c2a3d2e81be71ce48051
zhouyanqiang <zhouyanqiang@onething.net>
2020-03-09 12:51:40 +0800

    [add] 圆圈中最后剩下的数字

--- Data Structure and Algorithm/剑指offer代码/46. 孩子们的游戏(圆圈中最后剩下的数)/LastRemaining.py
+++ Data Structure and Algorithm/剑指offer代码/46. 孩子们的游戏(圆圈中最后剩下的数)/LastRemaining.py
@@ -1,5 +1,26 @@
 # -*- coding:utf-8 -*-
 
 class Solution:
-	def LastRemaining_Solution(self, n, m):
-		# write code here
+    def LastRemaining_Solution(self, n, m):
+        # write code here
+        n_list = [i for i in range(n)]
+        cursor = 0
+        while True:
+            temp_index = []
+            for i in range(0, len(n_list)):
+                if cursor == m - 1:
+                    temp_index.append(n_list[i])
+                    cursor = 0
+                else:
+                    cursor += 1
+
+            for j in temp_index:
+                n_list.remove(j)
+
+            if len(n_list) == 1:
+                return n_list[0]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.LastRemaining_Solution(11, 3))
