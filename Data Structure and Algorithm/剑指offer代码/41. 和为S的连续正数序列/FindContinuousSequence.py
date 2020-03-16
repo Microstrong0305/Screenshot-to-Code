39f2e8ba7214e0248085ec78d036947410ac766c
zhouyanqiang <zhouyanqiang@onething.net>
2020-03-16 14:10:51 +0800

    [add] 和为s的连续正数序列

--- Data Structure and Algorithm/剑指offer代码/41. 和为S的连续正数序列/FindContinuousSequence.py
+++ Data Structure and Algorithm/剑指offer代码/41. 和为S的连续正数序列/FindContinuousSequence.py
@@ -18,7 +18,43 @@ class Solution:
                     result_list.append(temp_list)
         return result_list
 
+    def FindContinuousSequence1(self, tsum):
+        if tsum == 0 or tsum == 1 or tsum == 2:
+            return []
+
+        result_list = []
+
+        mid = int((tsum + 1) // 2)
+        array = [i for i in range(0, mid + 1)]
+        small = 1
+        big = 2
+        temp_sum = array[small] + array[big]
+        temp = [array[small], array[big]]
+        while True:
+            if temp_sum == tsum:
+                temp_list = [t for t in temp]
+                result_list.append(temp_list)
+                if big < len(array) - 1:
+                    big += 1
+                    temp_sum = temp_sum + array[big]
+                    temp.append(array[big])
+                else:
+                    return result_list
+            if temp_sum > tsum:
+                temp_sum = temp_sum - array[small]
+                temp.remove(array[small])
+                small += 1
+            if temp_sum < tsum:
+                if big < len(array) - 1:
+                    big += 1
+                    temp_sum = temp_sum + array[big]
+                    temp.append(array[big])
+                else:
+                    return result_list
+
+        return result_list
+
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.FindContinuousSequence(3))
+    print(sol.FindContinuousSequence1(15))
