79541c085fcfc8671d8f2104fdcb7661b183f159
zhouyanqiang <zhouyanqiang@onething.net>
2020-03-20 11:08:56 +0800

    [add] 递归 and reduce

--- Data Structure and Algorithm/剑指offer代码/47. 求1+2+...+n/Sum_solution.py
+++ Data Structure and Algorithm/剑指offer代码/47. 求1+2+...+n/Sum_solution.py
@@ -1,24 +1,37 @@
+from functools import reduce
+
+
 class Temp(object):
+    N = 0
+    Sum = 0
 
-	N = 0
-	Sum = 0
+    def __init__(self):
+        Temp.N += 1
+        Temp.Sum += Temp.N
 
-	def __init__(self):
-		Temp.N += 1
-		Temp.Sum += Temp.N
-
-	def GetSum():
-		return Temp.Sum
-		
 
 class Solution:
-	def Sum_solution(self, n):
-		obj_list = [Temp() for i in range(n)]
+    def Sum_Solution(self, n):
+        obj_list = [Temp() for i in range(n)]
 
-		return Temp.GetSum()
+        return Temp.Sum
 
+    # 递归
+    def Sum_Solution1(self, n):
+        # write code here
+        if n == 0:
+            return 0
+        return n + self.Sum_Solution1(n - 1)
+
+    # reduce
+    def Sum_Solution2(self, n):
+        # write code here
+        def add(x, y):
+            return x + y
+
+        return reduce(add, range(1, n + 1))
 
 
 if __name__ == "__main__":
-	sol = Solution()
-	print(sol.Sum_solution(3))
+    sol = Solution()
+    print(sol.Sum_Solution1(5))
