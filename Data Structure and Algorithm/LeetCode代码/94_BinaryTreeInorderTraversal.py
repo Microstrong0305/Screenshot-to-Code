c5683f847ee157cf3f28daf531de3401a2678861
zhouyanqiang <zhouyanqiang@onething.net>
2020-03-27 13:46:43 +0800

    [add] 二叉树中序遍历迭代解法

--- Data Structure and Algorithm/LeetCode代码/94_BinaryTreeInorderTraversal.py
+++ Data Structure and Algorithm/LeetCode代码/94_BinaryTreeInorderTraversal.py
@@ -31,6 +31,38 @@ class Solution:
 
         return res
 
+    def inorderTraversal1(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return []
+        res, stack = [], []
+        stack.append(root)
+        while stack:
+            while root and root.left:
+                stack.append(root.left)
+                root = root.left
+
+            root = stack.pop()
+            res.append(root.val)
+
+            root = root.right
+
+            if root:
+                stack.append(root)
+
+        return res
+
+    def inorderTraversal2(self, root: TreeNode) -> List[int]:
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
+
 
 if __name__ == "__main__":
     root = TreeNode(1)
@@ -41,4 +73,4 @@ if __name__ == "__main__":
     level1_right.left = level2_left
 
     sol = Solution()
-    print(sol.inorderTraversal(root))
+    print(sol.inorderTraversal2(root))
