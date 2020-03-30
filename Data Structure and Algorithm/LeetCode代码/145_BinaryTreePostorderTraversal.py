d51b8846706cf314b964311e7cbf8370de0d9d38
zhouyanqiang <zhouyanqiang@onething.net>
2020-03-30 11:35:50 +0800

    [add] 二叉树后序遍历

---
+++ Data Structure and Algorithm/LeetCode代码/145_BinaryTreePostorderTraversal.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+
+        if root is None:
+            return []
+
+        res = []
+
+        def postorder(root, res):
+
+            if root is None:
+                return res
+
+            postorder(root.left, res)
+
+            postorder(root.right, res)
+
+            res.append(root.val)
+
+        postorder(root, res)
+
+        return res
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+level1_right = TreeNode(2)
+level2_left = TreeNode(3)
+
+root.right = level1_right
+level1_right.left = level2_left
+
+sol = Solution()
+print(sol.postorderTraversal(root))
