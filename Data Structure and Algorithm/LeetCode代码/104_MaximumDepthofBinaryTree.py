5965596575c1ff1ce69db5f688d6af193fc430b3
zhouyanqiang <zhouyanqiang@onething.net>
2020-04-02 10:51:34 +0800

    [add] 二叉树的最大深度

--- Data Structure and Algorithm/LeetCode代码/104_MaximumDepthofBinaryTree.py
+++ Data Structure and Algorithm/LeetCode代码/104_MaximumDepthofBinaryTree.py
@@ -27,6 +27,23 @@ class Solution:
 
         return getDepth(root)
 
+    def maxDepth2(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        stack = [(1, root)]
+
+        depth = 0
+
+        while stack:
+            current_depth, root = stack.pop()
+            if root:
+                depth = max(depth, current_depth)
+                stack.append((current_depth + 1, root.left))
+                stack.append((current_depth + 1, root.right))
+
+        return depth
+
 
 if __name__ == "__main__":
     root = TreeNode(3)
@@ -44,4 +61,4 @@ if __name__ == "__main__":
     R1.right = R2
 
     sol = Solution()
-    print(sol.maxDepth(root))
+    print(sol.maxDepth2(root))
